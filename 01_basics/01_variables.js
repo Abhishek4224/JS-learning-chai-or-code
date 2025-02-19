c9ffd4e45c6f24ef60559e3eba69c0ec0ffac407
Abhishek4224 <fitness44freek@gmail.com>
2025-02-19 16:26:07 +0000

    const and var and let are done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,1 +1,9 @@
+const accountid = 12345
+let accountmail = "fitness@mail.com"
+let accountpassword = "11223344"
+let accountcity = "churu"
+let accountstate;
+
+
+console.table([accountid, accountmail, accountpassword, accountcity, accountstate])
 
